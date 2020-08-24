3b8d2c27803340ea4b21e89a3642ef483d43fb28
Kimsangwon <kimsw509@g2e.co.kr>
2020-08-24 11:16:03 +0900

    test

--- lib/ui/widget/remain_stat_list_tile.dart
+++ lib/ui/widget/remain_stat_list_tile.dart
@@ -4,7 +4,6 @@ import 'package:mask/model/store.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class RemainStatListTile extends StatelessWidget {
-
   final Store store;
 
   RemainStatListTile(this.store);
@@ -12,59 +11,62 @@ class RemainStatListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(store.name?? ' '),
+      title: Text(store.name ?? ' '),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(store.addr ?? ' '),
-          Text('${store.km}')
-        ],
+        children: [Text(store.addr ?? ' '), Text('${store.km}')],
       ),
       trailing: _buildRemainStatWidget(store),
-      onTap: (){
+      onTap: () {
         print('tap');
-        _launchURL(store.lat,store.lng);
-        },
+        _launchURL(store.lat, store.lng);
+      },
     );
   }
 
-  Widget _buildRemainStatWidget(Store store){
-    var remainStat ='판매중지';
-    var description ='판매중지';
-    var color =Colors.black;
-    switch(store.remainStat){
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+    switch (store.remainStat) {
       case 'plenty':
-        remainStat ='충분';
-        description ='100개 이상';
-        color =Colors.green;
+        remainStat = '충분';
+        description = '120개 이상';
+        color = Colors.green;
         break;
       case 'some':
-        remainStat ='보통';
-        description ='30~100개';
-        color =Colors.yellow;
+        remainStat = '보통';
+        description = '30~100개';
+        color = Colors.yellow;
         break;
       case 'few':
-        remainStat ='부족';
-        description ='2~30개';
-        color =Colors.red;
+        remainStat = '부족';
+        description = '2~30개';
+        color = Colors.red;
         break;
       case 'empty':
-        remainStat ='소진임박';
-        description ='1개이하';
-        color =Colors.grey;
+        remainStat = '소진임박';
+        description = '1개이하';
+        color = Colors.grey;
         break;
-      default :
+      default:
     }
 
     return Column(
       children: [
-        Text(remainStat,style: TextStyle(color: color, fontWeight: FontWeight.bold),),
-        Text(description,style: TextStyle(color: color),),
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
       ],
     );
   }
 
-  _launchURL(double lat,double lng) async {
+  _launchURL(double lat, double lng) async {
     final url = 'https://google.com/maps/search/?api=1&query=$lat,$lng';
     if (await canLaunch(url)) {
       await launch(url);
@@ -73,5 +75,3 @@ class RemainStatListTile extends StatelessWidget {
     }
   }
 }
-
-
